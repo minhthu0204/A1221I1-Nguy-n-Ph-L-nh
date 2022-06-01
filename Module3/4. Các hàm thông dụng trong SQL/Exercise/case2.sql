d55fada66ba3ea573df1a2753f1e75f4661ef885
minhthu0204 <phulinhnguyen3@gmail.com>
2022-06-01 19:31:11 +0700

    haha

--- Module3/4. Các hàm thông dụng trong SQL/Exercise/case2.sql
+++ Module3/4. Các hàm thông dụng trong SQL/Exercise/case2.sql
@@ -25,7 +25,7 @@ where ten_loai_khach = 'diamond'
 group by khach_hang.ma_khach_hang
 order by so_lan_dat_phong;
 
- /*5. 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong,
+ /*5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong,
  ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, 
  với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
  cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).	 */  
